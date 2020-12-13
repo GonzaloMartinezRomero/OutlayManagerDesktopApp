65f6fa6c784352fa1c5616f2e2d2b997bdbe3ecd
Gonzalo <Gonzalo@correo.com>
2020-12-13 20:39:59 +0100

    Transacions analycer view

---
+++ OutlayManager_DesktopApp/OutlayManagerWF/View/TransactionsAnalycer/TransactionAnalycer.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace OutlayManagerWF.View.TransactionsAnalycer
+{
+    partial class TransactionAnalycer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.transactionsChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.textBoxTotalAmount = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.transactionsDataGridView = new Zuby.ADGV.AdvancedDataGridView();
+            this.chartTypeSelector = new System.Windows.Forms.ComboBox();
+            this.buttonChangeCharType = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.transactionsChart)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.transactionsDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.chartTypeSelector);
+            this.splitContainer1.Panel1.Controls.Add(this.buttonChangeCharType);
+            this.splitContainer1.Panel1.Controls.Add(this.textBoxTotalAmount);
+            this.splitContainer1.Panel1.Controls.Add(this.textBox1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.splitContainer2);
+            this.splitContainer1.Size = new System.Drawing.Size(1554, 602);
+            this.splitContainer1.SplitterDistance = 55;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.transactionsDataGridView);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.transactionsChart);
+            this.splitContainer2.Size = new System.Drawing.Size(1554, 543);
+            this.splitContainer2.SplitterDistance = 589;
+            this.splitContainer2.TabIndex = 0;
+            // 
+            // transactionsChart
+            // 
+            this.transactionsChart.BackColor = System.Drawing.Color.Transparent;
+            chartArea1.AlignmentOrientation = ((System.Windows.Forms.DataVisualization.Charting.AreaAlignmentOrientations)((System.Windows.Forms.DataVisualization.Charting.AreaAlignmentOrientations.Vertical | System.Windows.Forms.DataVisualization.Charting.AreaAlignmentOrientations.Horizontal)));
+            chartArea1.AlignmentStyle = System.Windows.Forms.DataVisualization.Charting.AreaAlignmentStyles.None;
+            chartArea1.Area3DStyle.IsClustered = true;
+            chartArea1.Area3DStyle.IsRightAngleAxes = false;
+            chartArea1.Area3DStyle.LightStyle = System.Windows.Forms.DataVisualization.Charting.LightStyle.Realistic;
+            chartArea1.Area3DStyle.PointDepth = 50;
+            chartArea1.Area3DStyle.PointGapDepth = 150;
+            chartArea1.Area3DStyle.WallWidth = 5;
+            chartArea1.BackColor = System.Drawing.Color.Honeydew;
+            chartArea1.BackImageTransparentColor = System.Drawing.Color.Transparent;
+            chartArea1.IsSameFontSizeForAllAxes = true;
+            chartArea1.Name = "ChartArea1";
+            this.transactionsChart.ChartAreas.Add(chartArea1);
+            this.transactionsChart.Dock = System.Windows.Forms.DockStyle.Fill;
+            legend1.Name = "Legend1";
+            this.transactionsChart.Legends.Add(legend1);
+            this.transactionsChart.Location = new System.Drawing.Point(0, 0);
+            this.transactionsChart.Name = "transactionsChart";
+            this.transactionsChart.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Excel;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.SplineArea;
+            series1.IsValueShownAsLabel = true;
+            series1.IsXValueIndexed = true;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            series1.YValuesPerPoint = 2;
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.SplineArea;
+            series2.Legend = "Legend1";
+            series2.Name = "Series2";
+            this.transactionsChart.Series.Add(series1);
+            this.transactionsChart.Series.Add(series2);
+            this.transactionsChart.Size = new System.Drawing.Size(961, 543);
+            this.transactionsChart.TabIndex = 1;
+            this.transactionsChart.Text = "chart1";
+            // 
+            // textBoxTotalAmount
+            // 
+            this.textBoxTotalAmount.Location = new System.Drawing.Point(91, 16);
+            this.textBoxTotalAmount.Name = "textBoxTotalAmount";
+            this.textBoxTotalAmount.ReadOnly = true;
+            this.textBoxTotalAmount.Size = new System.Drawing.Size(142, 22);
+            this.textBoxTotalAmount.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(21, 16);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(64, 22);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "Total";
+            // 
+            // transactionsDataGridView
+            // 
+            this.transactionsDataGridView.AllowUserToAddRows = false;
+            this.transactionsDataGridView.AllowUserToDeleteRows = false;
+            this.transactionsDataGridView.AllowUserToOrderColumns = true;
+            this.transactionsDataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.transactionsDataGridView.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.DisplayedCells;
+            this.transactionsDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.transactionsDataGridView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.transactionsDataGridView.FilterAndSortEnabled = true;
+            this.transactionsDataGridView.Location = new System.Drawing.Point(0, 0);
+            this.transactionsDataGridView.Name = "transactionsDataGridView";
+            this.transactionsDataGridView.ReadOnly = true;
+            this.transactionsDataGridView.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.transactionsDataGridView.RowHeadersWidth = 51;
+            this.transactionsDataGridView.RowTemplate.Height = 24;
+            this.transactionsDataGridView.Size = new System.Drawing.Size(589, 543);
+            this.transactionsDataGridView.TabIndex = 5;
+            // 
+            // chartTypeSelector
+            // 
+            this.chartTypeSelector.FormattingEnabled = true;
+            this.chartTypeSelector.Location = new System.Drawing.Point(588, 14);
+            this.chartTypeSelector.Name = "chartTypeSelector";
+            this.chartTypeSelector.Size = new System.Drawing.Size(145, 24);
+            this.chartTypeSelector.TabIndex = 9;
+            // 
+            // buttonChangeCharType
+            // 
+            this.buttonChangeCharType.Location = new System.Drawing.Point(739, 12);
+            this.buttonChangeCharType.Name = "buttonChangeCharType";
+            this.buttonChangeCharType.Size = new System.Drawing.Size(145, 26);
+            this.buttonChangeCharType.TabIndex = 8;
+            this.buttonChangeCharType.Text = "Change ";
+            this.buttonChangeCharType.UseVisualStyleBackColor = true;
+            this.buttonChangeCharType.Click += new System.EventHandler(this.buttonCalculateSaving_Click);
+            // 
+            // TransactionAnalycer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1554, 602);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "TransactionAnalycer";
+            this.Text = "Transaction Analycer";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.transactionsChart)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.transactionsDataGridView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart transactionsChart;
+        private System.Windows.Forms.TextBox textBoxTotalAmount;
+        private System.Windows.Forms.TextBox textBox1;
+        private Zuby.ADGV.AdvancedDataGridView transactionsDataGridView;
+        private System.Windows.Forms.ComboBox chartTypeSelector;
+        private System.Windows.Forms.Button buttonChangeCharType;
+    }
+}
---
+++ OutlayManager_DesktopApp/OutlayManagerWF/View/TransactionsAnalycer/TransactionAnalycer.cs
@@ -0,0 +1,127 @@
+﻿using FastMember;
+using OutlayManagerWF.Manager;
+using OutlayManagerWF.Model.Info;
+using OutlayManagerWF.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using Zuby.ADGV;
+
+namespace OutlayManagerWF.View.TransactionsAnalycer
+{
+    public partial class TransactionAnalycer : Form
+    {
+        public TransactionAnalycer()
+        {
+            InitializeComponent();
+            this.transactionsDataGridView.FilterStringChanged += FilterStringChanged;
+            InitializeChart();
+        }
+
+        private void InitializeChart()
+        {
+           this.chartTypeSelector.Items.AddRange(new object[]
+           {
+                SeriesChartType.Line,
+                SeriesChartType.Spline,
+                SeriesChartType.Area,
+                SeriesChartType.Column,
+                SeriesChartType.Pie
+           });
+
+            //Line by default
+            this.chartTypeSelector.SelectedItem = SeriesChartType.Line;
+        }
+
+        public void LoadTransactions()
+        {
+            try
+            {
+                List<ResumeTransactionDTO> transactions = null;
+                transactions = new TransactionManager().GetAllTransactions();
+
+                DataTable dataTable = new DataTable();
+
+                using (var reader = ObjectReader.Create(transactions))
+                {
+                    dataTable.Load(reader);
+                }
+
+                BindingSource bindingSource = new BindingSource
+                {
+                    DataSource = dataTable
+                };
+
+                this.transactionsDataGridView.DataSource = bindingSource;
+            }
+            catch (Exception e)
+            {
+                new DialogManager().ShowDialog(DialogManager.DialogLevel.Exception, e.Message, this);
+            }
+        }   
+
+        private void FilterStringChanged(object sender, AdvancedDataGridView.FilterEventArgs e)
+        {   
+            if(sender is AdvancedDataGridView dataGrid)
+            {
+                BindingSource bindingSource = dataGrid.DataSource as BindingSource;
+                DataView dataView = bindingSource.List as DataView;
+                dataView.RowFilter = e.FilterString;
+
+                SumTransactionsValues(dataView);
+                GraphicTransactionsChart(dataView);
+            }   
+        }
+
+        private void GraphicTransactionsChart(DataView dataView)
+        {
+            this.transactionsChart.Series.Clear();
+
+            List<DataRowView> dataRowFiltered = new List<DataRowView>();
+            foreach (DataRowView row in dataView)
+                dataRowFiltered.Add(row);
+
+            var codeGrouping = dataRowFiltered.GroupBy(row => row[nameof(ResumeTransactionDTO.Code)]);
+
+            foreach (var transactionCodes in codeGrouping)
+            {
+                string groupName = transactionCodes.Key.ToString();
+                this.transactionsChart.Series.Add(groupName);
+
+                var dataRows = transactionCodes.Select(x => x).ToList();
+
+                foreach (var dateTimeTransaction in dataRows)
+                {
+                    DateTime dt = CastObject.ToDateTime(dateTimeTransaction[nameof(ResumeTransactionDTO.Date)]);
+                    double amount = CastObject.ToDouble(dateTimeTransaction[nameof(ResumeTransactionDTO.Amount)]);
+                  
+                    this.transactionsChart.Series[groupName].Points.AddXY(dt, amount);
+                }
+            }
+        }
+
+        private void SumTransactionsValues(DataView dataView)
+        {
+            double totalAmountFiteredColumns = 0.0d;
+
+            foreach (DataRowView dataRowFiltered in dataView)
+            {
+                totalAmountFiteredColumns += Utilities.Normalizer.NormalizeAmount(dataRowFiltered[nameof(ResumeTransactionDTO.Amount)]);
+            }
+
+            this.textBoxTotalAmount.Text = Utilities.Normalizer.SpainFormatAmount(totalAmountFiteredColumns);
+        }
+
+        private void buttonCalculateSaving_Click(object sender, EventArgs e)
+        {
+            SeriesChartType chartTypeSelected = this.chartTypeSelector.SelectedItem as SeriesChartType? ?? SeriesChartType.Line;
+
+            foreach (var serieAux in this.transactionsChart.Series)
+                serieAux.ChartType = chartTypeSelected;
+        }
+    }
+}
+    
